deba333e8635ae2bcf8d2a1e0b14a3451251e0cd
Jose Angel <jaalvarez2818@gmail.com>
2026-05-21 02:38:18 +0200

    Setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sythonlab_travel_repo",
-    version="0.0.1",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[],
     url="https://github.com/sythonlab/SythonLab-Travel-Repo",
